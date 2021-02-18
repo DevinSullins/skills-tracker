6cfd925097893944bb4fa2b9dc820e19fea89227
Devin Sullins <devinsullins@gmail.com>
2021-02-17 19:46:45 -0600

    Initial commit

---
+++ src/main/java/org/launchcode/controllers/SkillsController.java
@@ -0,0 +1,12 @@
+package org.launchcode.controllers;
+
+public class SkillsController {
+
+    //TO DO: At localhost:8080, add text that states the three possible programming languages someone could be working on. You need to have an h1 with the title “Skills Tracker”, an h2, and an ol containing three programming languages of your choosing.
+
+
+    //At localhost:8080/form, add a form that lets the user enter their name and choose their favorite, second favorite, and third favorite programming languages on your list. Use select elements for each of the rankings. Just as with the exercises, we will use @GetMapping().
+
+
+    //Also at localhost:8080/form, use @PostMapping and @RequestParam to update the HTML with an h1 stating the user’s name and an ol showing the three programming languages in the order they chose.
+}
